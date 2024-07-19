81e7db8132165411494b768ebc6a1ee374041420
RAMYA <104247634+RamyaM9645@users.noreply.github.com>
2024-07-19 15:33:37 +0530

    Add files via upload

---
+++ person_main.java
@@ -0,0 +1,91 @@
+//perosn=>employee=>teacher use array of objects to display deials of n teacher
+import java.util.*;
+class Personn
+{
+    Scanner sc=new Scanner(System.in);
+    String name,address;
+    char gender;
+    int age;
+    Personn(int x)
+    {
+        System.out.println("\n\n\t\tEnter details of Teacher "+x);
+        System.out.print("Enter Name: ");
+        name=sc.next();
+        System.out.print("Enter Gender(M/F/O): ");
+        gender=sc.next().charAt(0);
+        System.out.print("Enter Age: ");
+        age=sc.nextInt();        
+        System.out.print("Enter Address: ");
+        address=sc.next();
+    }
+}
+
+class Eemploy extends Person
+{
+    Scanner sc=new Scanner(System.in);
+    int EmpId;
+    String CompanyName,qualification;
+    double salary;
+    Eemploy(int x)
+    {
+        super(x);
+        System.out.print("Enter Employee ID: ");
+        EmpId=sc.nextInt();
+        System.out.print("Enter Company Name: ");
+        CompanyName=sc.next();
+        System.out.print("Enter Qualification: ");
+        qualification=sc.next();
+        System.out.print("Enter Salary: ");
+        salary=sc.nextDouble();
+    }
+}
+
+
+class Teacherrr extends Eemploy
+{
+    Scanner sc=new Scanner(System.in);
+    String subject,department;
+    int TeacherId;
+    Teacherrr(int x)
+    {
+        super(x);
+        System.out.print("Enter Teacher ID: ");
+        TeacherId=sc.nextInt();
+        System.out.print("Enter Subject: ");
+        subject=sc.next();
+        System.out.print("Enter Department: ");
+        department=sc.next();
+    }
+    void display(int n)
+    {
+        System.out.println("\n\n\t\tTeacher "+n+" Details");
+        System.out.println("Name: "+name);
+        System.out.println("Gender: "+gender);
+        System.out.println("Address: "+address);
+        System.out.println("Age: "+age);
+        System.out.println("Employee ID: "+EmpId);
+        System.out.println("Teacher ID: "+name);
+        System.out.println("Company Name: "+CompanyName);
+        System.out.println("Department: "+name);
+        System.out.println("Qualification: "+qualification);
+        System.out.println("Salary: "+salary);
+        System.out.println("Subject: "+subject);
+
+    }
+}
+
+class person_main
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter no:of teachers: ");
+        int n =sc.nextInt();
+        Teacherrr t[]=new Teacherrr[n];
+        for(int i=0;i<n;i++)
+            t[i]=new Teacherrr(i+1);
+        for(int i=0;i<n;i++)
+            t[i].display(i+1);
+    }
+    
+}
